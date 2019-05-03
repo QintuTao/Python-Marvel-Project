d6f7135ec982d707e1f0234f88e38a7b035043d3
Qintu <tqt010114@icloud.com>
2019-05-03 10:06:42 -0700

    print statement added

--- Python-Marvel-Project/main.py
+++ Python-Marvel-Project/main.py
@@ -4,7 +4,5 @@ PUBLIC_KEY = "75f9ed43f23271b39b0257e422c150b3"
 PRIVATE_KEY = "068e03d27a588b3a806e9201f539ca81a92d31c5"
 
 if __name__ == '__main__':
-    print("Marvel Project Started")
-
-
+    print("what the fuck")
 
