474c2b71436e9d6932998fba570641529836f4e2
Qintu <tqt010114@icloud.com>
2019-05-04 17:06:48 -0700

    print statemtent changed

--- Python-Marvel-Project/main.py
+++ Python-Marvel-Project/main.py
@@ -4,6 +4,17 @@ from marvel import Marvel
 PUBLIC_KEY = "75f9ed43f23271b39b0257e422c150b3"
 PRIVATE_KEY = "068e03d27a588b3a806e9201f539ca81a92d31c5"
 
-if __name__ == '__main__':
-    print("what the fuck")
+# Fetch Data from Marvel Database
+marvel = Marvel(PUBLIC_KEY,PRIVATE_KEY)
 
+# Get specific data from the request results
+characters = marvel.characters
+comics = marvel.comics
+series = marvel.series
+creator = marvel.creators
+story = marvel.stories
+events = marvel.events
+
+
+if __name__ == '__main__':
+    print(comics.all())
